3ba0e08d45ebe12321fbbcc6f393e5984b70f648
PonderSlime (PC) <micah.j.edwards1@gmail.com>
2025-04-10 16:56:59 -0600

    App Init

---
+++ composeApp/src/desktopMain/kotlin/io/ponderslime/snotes/main.kt
@@ -0,0 +1,13 @@
+package io.ponderslime.snotes
+
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+
+fun main() = application {
+    Window(
+        onCloseRequest = ::exitApplication,
+        title = "SNotes",
+    ) {
+        App()
+    }
+}
---
+++ composeApp/src/iosMain/kotlin/io/ponderslime/snotes/MainViewController.kt
@@ -0,0 +1,5 @@
+package io.ponderslime.snotes
+
+import androidx.compose.ui.window.ComposeUIViewController
+
+fun MainViewController() = ComposeUIViewController { App() }
